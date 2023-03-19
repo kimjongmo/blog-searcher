a7521c9a827d29afa24118a2b3b333b234ae09e3
kimjongmo <kfmd1008@naver.com>
2023-03-19 19:55:16 +0900

    test setting & write test code

--- infra/client/build.gradle.kts
+++ infra/client/build.gradle.kts
@@ -1,9 +1,17 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
+    id("org.springframework.boot") version "2.7.9"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
 }
 
 repositories {
     mavenCentral()
+}
+
+tasks.withType<BootJar> {
+    enabled = false
 }
 
 dependencies {
