f20ea8332ab00aaa62497c8174040a846b78181a
kimjongmo <kfmd1008@naver.com>
2023-03-20 01:20:11 +0900

    spring framework base proejct

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
+    id("org.springframework.boot") version "2.7.9"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
     id("org.jmailen.kotlinter") version "3.10.0"
 }
 
@@ -9,9 +13,17 @@ repositories {
     mavenCentral()
 }
 
+tasks.withType<BootJar> {
+    enabled = false
+}
+
 subprojects {
     apply(plugin = "kotlin")
     apply(plugin = "org.jmailen.kotlinter")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
 
     group = "com.example.blogsearcher"
     version = "0.0.1-SNAPSHOT"
@@ -24,6 +36,9 @@ subprojects {
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+        implementation("org.springframework.boot:spring-boot-starter")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
         testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     }
@@ -37,5 +52,9 @@ subprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+    }
+
+    tasks.withType<BootJar> {
+        enabled = false
     }
 }
