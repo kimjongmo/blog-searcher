156c0697fa714a639f08581ca221350b71f3d9b2
kimjongmo <kfmd1008@naver.com>
2023-03-19 19:53:39 +0900

    date time with zone de/serializer

--- infra/client/src/main/kotlin/com/example/blogsearcher/infra/client/kakao/factory/KakaoClientFactory.kt
+++ infra/client/src/main/kotlin/com/example/blogsearcher/infra/client/kakao/factory/KakaoClientFactory.kt
@@ -5,10 +5,6 @@ import com.example.blogsearcher.infra.client.core.factory.SimpleRetrofitFactory
 import com.example.blogsearcher.infra.client.core.interceptor.HeaderInterceptor
 import com.example.blogsearcher.infra.client.core.interceptor.RetryInterceptor
 import com.example.blogsearcher.infra.client.kakao.KakaoClient
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonDeserializer
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.module.SimpleModule
@@ -17,7 +13,6 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import okhttp3.Interceptor
 import okhttp3.logging.HttpLoggingInterceptor
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class KakaoClientFactory(properties: RetrofitProperties) : SimpleRetrofitFactory<KakaoClient>(properties) {
     override fun initInterceptors(): List<Interceptor> {
@@ -27,22 +22,13 @@ class KakaoClientFactory(properties: RetrofitProperties) : SimpleRetrofitFactory
     }
 
     override fun initObjectMapper(): ObjectMapper {
-
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
-
-        val timeModule = JavaTimeModule().addDeserializer(
-            LocalDateTime::class.java,
-            object : JsonDeserializer<LocalDateTime>() {
-                override fun deserialize(p: JsonParser, ctxt: DeserializationContext): LocalDateTime {
-                    val node = p.codec.readTree<JsonNode>(p)
-                    val str = node.textValue()
-                    return LocalDateTime.parse(str, formatter)
-                }
-            }
-        )
+        val javaTimeModule = JavaTimeModule().apply {
+            addDeserializer(LocalDateTime::class.java, LocalDateTimeWithZoneDeserializer())
+            addSerializer(LocalDateTime::class.java, LocalDateTimeWithZoneSerializer())
+        }
 
         return ObjectMapper()
-            .registerModules(timeModule, SimpleModule(), KotlinModule.Builder().build())
+            .registerModules(javaTimeModule, SimpleModule(), KotlinModule.Builder().build())
             .also { it.propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE }
     }
 }
