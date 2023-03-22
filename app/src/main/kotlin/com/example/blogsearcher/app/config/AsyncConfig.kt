48a2cc4823afa921cbae9b4617f43739cbee68c5
kimjongmo <kfmd1008@naver.com>
2023-03-22 13:57:28 +0900

    async error logging

--- app/src/main/kotlin/com/example/blogsearcher/app/config/AsyncConfig.kt
+++ app/src/main/kotlin/com/example/blogsearcher/app/config/AsyncConfig.kt
@@ -1,10 +1,13 @@
 package com.example.blogsearcher.app.config
 
+import mu.KotlinLogging
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.lang.reflect.Method
 
 @Configuration
 @EnableAsync
@@ -27,4 +30,15 @@ class AsyncConfig : AsyncConfigurer {
         }.also {
             it.initialize()
         }
+
+    override fun getAsyncUncaughtExceptionHandler(): AsyncUncaughtExceptionHandler? {
+        return CustomAsyncUncaughtExceptionHandler()
+    }
+
+    class CustomAsyncUncaughtExceptionHandler : AsyncUncaughtExceptionHandler {
+        private val log = KotlinLogging.logger { }
+        override fun handleUncaughtException(ex: Throwable, method: Method, vararg params: Any?) {
+            log.error("method name = ${method.name}, ${ex.message}", ex)
+        }
+    }
 }
