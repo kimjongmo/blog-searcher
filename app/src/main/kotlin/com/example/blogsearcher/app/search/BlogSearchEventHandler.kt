7f0e2ccb666993cbd0111b8478af23f4ecf8e758
kimjongmo <kfmd1008@naver.com>
2023-03-22 14:02:03 +0900

    with lock

--- app/src/main/kotlin/com/example/blogsearcher/app/search/BlogSearchEventHandler.kt
+++ app/src/main/kotlin/com/example/blogsearcher/app/search/BlogSearchEventHandler.kt
@@ -8,12 +8,15 @@ import com.github.benmanes.caffeine.cache.LoadingCache
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 @Component
 class BlogSearchEventHandler(
     private val blogSearchEventRepository: BlogSearchRecordRepository,
     private val cache: LoadingCache<String, Long>
 ) {
+    private val lock = ReentrantLock()
     /**
      * 검색 이벤트 발생시 데이터베이스에 저장.
      * 검색 응답에 영향을 주지 않게하기 위해 비동기로 실행시키도록 한다
@@ -32,7 +35,10 @@ class BlogSearchEventHandler(
     @EventListener(BlogSearchEvent::class)
     fun saveOrIncrease(blogSearchEvent: BlogSearchEvent) {
         val keyword = blogSearchEvent.keyword.value
-        val count = cache.get(keyword)
-        cache.put(keyword, count + 1)
+
+        lock.withLock {
+            val count = cache.get(keyword)
+            cache.put(keyword, count + 1)
+        }
     }
 }
